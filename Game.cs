f3a3c3228775adbc017cc10c150251c144005912
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-04-25 13:02:20 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_DeBord_Joshua
+{
+    class Game
+    {
+        public string title, genre;
+
+        //I don't know how to explain what these do exactly in my own words but i do have it in my head 
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {
+            title = titleParam;
+            genre = genreParam;
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors_DeBord_Joshua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game myGame = new Game("CS:GO", "FPS");
+
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game."); //tells the user my favorite game and it's genre
+
+            Game myOtherGame = new Game();
+
+            Console.WriteLine($"My favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game."); //tells the user my 2nd favorite game and its drama
+
+        }
+    }
+}
